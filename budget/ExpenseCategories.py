51fc00c5e08bbc521d9ff0f54001a33093216d9b
Badal Priyadarshi <priyadarshibadal.gmi@gmail.com>
2022-07-05 07:16:07 +0530

    modufued setup

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -17,11 +17,21 @@ def main():
             print("Sets are NOT equal by subset test")
 
     print(timeit.timeit(stmt="expenses.categorize_for_loop()",
-                        setup='''from . import Expense \nexpenses = Expense.Expenses() \nexpenses.read_expenses('data/spending_data.csv')''',
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
                         number=100000, globals=globals()))
 
     print(timeit.timeit(stmt="expenses.categorize_set_comprehension()",
-                        setup='''from . import Expense \nexpenses = Expense.Expenses() \nexpenses.read_expenses('data/spending_data.csv')''',
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
                         number=100000, globals=globals()))
 
     fig, ax = plt.subplots()
